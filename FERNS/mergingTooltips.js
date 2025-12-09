34d7ed6f21288ad4a1d9beb74c915f7539856f1c
Joanna Merson <jmerson@uoregon.edu>
2025-12-09 15:10:27 -0800

    revert tooltip changes

--- FERNS/mergingTooltips.js
+++ FERNS/mergingTooltips.js
@@ -13,68 +13,71 @@ function mergeTooltips(slider, threshold, separator) {
 
   // Move tooltips into the origin element. The default stylesheet handles this.
   tooltips.forEach(function (tooltip, index) {
-      if (tooltip) {
-          origins[index].appendChild(tooltip);
-      }
+    if (tooltip) {
+      origins[index].appendChild(tooltip);
+    }
   });
 
   slider.noUiSlider.on('update', function (values, handle, unencoded, tap, positions) {
 
-      var pools = [[]];
-      var poolPositions = [[]];
-      var poolValues = [[]];
-      var atPool = 0;
+    var pools = [[]];
+    var poolPositions = [[]];
+    var poolValues = [[]];
+    var atPool = 0;
 
-      // Assign the first tooltip to the first pool, if the tooltip is configured
-      if (tooltips[0]) {
-          pools[0][0] = 0;
-          poolPositions[0][0] = positions[0];
-          poolValues[0][0] = values[0];
+    // Assign the first tooltip to the first pool, if the tooltip is configured
+    if (tooltips[0]) {
+      pools[0][0] = 0;
+      poolPositions[0][0] = positions[0];
+      poolValues[0][0] = values[0];
+    }
+
+    for (var i = 1; i < positions.length; i++) {
+      if (!tooltips[i] || (positions[i] - positions[i - 1]) > threshold) {
+        atPool++;
+        pools[atPool] = [];
+        poolValues[atPool] = [];
+        poolPositions[atPool] = [];
       }
 
-      for (var i = 1; i < positions.length; i++) {
-          if (!tooltips[i] || (positions[i] - positions[i - 1]) > threshold) {
-              atPool++;
-              pools[atPool] = [];
-              poolValues[atPool] = [];
-              poolPositions[atPool] = [];
-          }
-
-          if (tooltips[i]) {
-              pools[atPool].push(i);
-              poolValues[atPool].push(values[i]);
-              poolPositions[atPool].push(positions[i]);
-          }
+      if (tooltips[i]) {
+        pools[atPool].push(i);
+        poolValues[atPool].push(values[i]);
+        poolPositions[atPool].push(positions[i]);
       }
+    }
 
-      pools.forEach(function (pool, poolIndex) {
-          var handlesInPool = pool.length;
+    pools.forEach(function (pool, poolIndex) {
+      var handlesInPool = pool.length;
 
-          for (var j = 0; j < handlesInPool; j++) {
-              var handleNumber = pool[j];
+      for (var j = 0; j < handlesInPool; j++) {
+        var handleNumber = pool[j];
 
-              if (j === handlesInPool - 1) {
-                  var offset = 0;
+        if (j === handlesInPool - 1) {
+          var offset = 0;
 
-                  poolPositions[poolIndex].forEach(function (value) {
-                      offset += 1000 - value;
-                  });
+          poolPositions[poolIndex].forEach(function (value) {
+            offset += 1000 - value;
+          });
 
-                  var direction = isVertical ? 'bottom' : 'right';
-                  var last = isRtl ? 0 : handlesInPool - 1;
-                  var lastOffset = 1000 - poolPositions[poolIndex][last];
-                  offset = (textIsRtl && !isVertical ? 100 : 0) + (offset / handlesInPool) - lastOffset;
+          var direction = isVertical ? 'bottom' : 'right';
+          var last = isRtl ? 0 : handlesInPool - 1;
+          var lastOffset = 1000 - poolPositions[poolIndex][last];
+          offset = (textIsRtl && !isVertical ? 100 : 0) + (offset / handlesInPool) - lastOffset;
 
-                  // Center this tooltip over the affected handles
-                  tooltips[handleNumber].innerHTML = poolValues[poolIndex].join(separator);
-                  tooltips[handleNumber].style.display = 'block';
-                  tooltips[handleNumber].style[direction] = offset + '%';
-              } else {
-                  // Hide this tooltip
-                  tooltips[handleNumber].style.display = 'none';
-              }
-          }
-      });
+          // Clamp offset to prevent tooltip overflow (min 10%, max 90%)
+          offset = Math.max(10, Math.min(90, offset));
+
+          // Center this tooltip over the affected handles
+          tooltips[handleNumber].innerHTML = poolValues[poolIndex].join(separator);
+          tooltips[handleNumber].style.display = 'block';
+          tooltips[handleNumber].style[direction] = offset + '%';
+        } else {
+          // Hide this tooltip
+          tooltips[handleNumber].style.display = 'none';
+        }
+      }
+    });
   });
 }
 
